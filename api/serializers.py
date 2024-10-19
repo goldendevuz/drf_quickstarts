6d1626fd9b2b06723d41ef188e79a57c605c54d8
uzinfocomga <uzinfocomga@gmail.com>
2024-10-19 20:34:42 +0500

    first commit

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,18 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from drf_compound_fields.fields import ListField
+from drf_compound_fields.fields import ListField, ListOrItemField, DictField, PartialDictField
 
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    # This is the new stuff:
     phone_numbers = ListField(child=serializers.CharField(), allow_empty=True)
+    skills = ListField(child=serializers.CharField(), allow_empty=True)  # E.g., ["javascript", "python", "ruby"]
+    username = ListOrItemField(serializers.CharField())  # E.g., "Prince" or ["John", "Smith"]
+    bookmarks = DictField(child=serializers.URLField(), allow_empty=True)  # E.g., {"./": "http://slashdot.org"}
+    measurements = PartialDictField(child=serializers.IntegerField(), included_keys=['height', 'weight'])
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'phone_numbers']
+        # fields = '__all__' # Not recommended for this model
+        fields = ['id', 'username', 'email', 'phone_numbers', 'bookmarks', 'measurements', 'skills']
