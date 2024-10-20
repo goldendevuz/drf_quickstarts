bc421dc06527f7a9655d0618808121ba114900b2
uzinfocomga <uzinfocomga@gmail.com>
2024-10-20 11:07:45 +0500

    first commit

--- config/settings.py
+++ config/settings.py
@@ -119,3 +119,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework_rapidjson.renderers.RapidJSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework_rapidjson.parsers.RapidJSONParser',
+    )
+}
