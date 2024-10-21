eb047a5543a81c5ca03a8c75688d389adc307a97
uzinfocomga <uzinfocomga@gmail.com>
2024-10-21 20:23:23 +0500

    first commit

--- api/admin.py
+++ api/admin.py
@@ -1,1 +1,5 @@
 # Register your models here.
+from django.contrib import admin
+from sendables.core.models import ReceivedSendable, RecipientSendableAssociation
+
+admin.site.register([ReceivedSendable, RecipientSendableAssociation])
--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # ...,
+    path("sendables/", include("sendables.urls")),
     path('', include('api.urls')),
 ]
